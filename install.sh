9bef01c3ed2244ab26fe41cf3946b65f3c6d635e
Remi Nyborg <remi@nyborg.priv.no>
2017-11-13 08:24:31 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Get current dir (so run this script from anywhere)
+
+export DOTFILES_DIR DOTFILES_CACHE DOTFILES_EXTRA_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Make utilities available
+
+PATH="$DOTFILES_DIR/bin:$PATH"
+
+# Update dotfiles itself first
+
+if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
+
+# Bunch of symlinks
+
+ln -sfv "$DOTFILES_DIR/system/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/.vimrc" ~
+ln -sfv ~/.vimrc ~/.config/nvim/init.vim
